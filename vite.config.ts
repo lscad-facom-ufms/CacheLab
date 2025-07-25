e5edaf7187579bbff584c944bf80bee098249128
shino <shino@evop.com.br>
2025-07-25 18:40:33 -0400

    Ajustes para git-page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,5 @@ export default defineConfig({
     globals: true,
   },
   plugins: [react()],
-})
+  base: "/CacheLab",
+});
